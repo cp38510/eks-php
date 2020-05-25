46b2cf5312177d66dc23ca59b788d0f6a9652e7d
Andrey Reshetnikov <a.s.reshetnikov@s7.ru>
2020-05-25 13:24:58 +0300

    first commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "The time in " . date_default_timezone_get() . " is " . date("H:i:s") . "<br>";
 
-echo "Hostname pod is: " . gethostname();
+echo "Hostname pod is: " .  gethostname() . "<br>";
 
 echo phpinfo();
 ?>
