e9f51694c94842ca15a93f8082ba427beb8d569c
Andrey Reshetnikov <a.s.reshetnikov@s7.ru>
2020-05-25 12:40:36 +0300

    first commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "The time in " . date_default_timezone_get() . " is " . date("H:i:s") . "<br>";
-echo "Random number is: "(rand(1, 1000000) . "<br>");
+echo "Random number is: " . (rand(1, 1000000) . "<br>");
 echo phpinfo();
 ?>
 
