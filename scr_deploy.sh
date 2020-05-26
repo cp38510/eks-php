fc611dff075fc67577d275e4285d4e6703ba2465
Andrey Reshetnikov <a.s.reshetnikov@s7.ru>
2020-05-26 09:09:54 +0300

    fix delete cluster

--- scr_deploy.sh
+++ scr_deploy.sh
@@ -12,6 +12,9 @@ terraform apply -auto-approve terraform-provision-eks-cluster
 #Get ~/.kube/config
 aws eks --region eu-central-1 update-kubeconfig --name $(aws eks list-clusters |grep php | cut -d'"' -f2)
 
+#Check install
+terraform apply -auto-approve terraform-provision-eks-cluster
+
 #Install service
 helm install eks-php ./php-app-chart
 
