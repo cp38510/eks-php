857c57e8dd569156e7cda69335d3d82b22f85aea
Andrey Reshetnikov <a.s.reshetnikov@s7.ru>
2020-05-26 21:48:36 +0300

    change scr_check_up.sh

--- scr_check_up.sh
+++ scr_check_up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DOMAIN="$(kubectl get svc |grep LoadBalancer |awk '{print $4}')"
 
+
 aws eks list-clusters |grep -c php
 if [ $? -ne 0 ]; then echo "ERROR, EKS cluster not exist, check: aws eks list-clusters" && exit 1; fi
 
@@ -13,13 +14,15 @@ do
 done
 
 
-until $(curl -s --fail $DOMAIN |grep HOSTNAME |grep -c "php-" > /dev/null); do
+RESULT="$(curl -s $DOMAIN |grep HOSTNAME | grep -c $(kubectl get pod |grep 'php-' |awk '{print $1}'))"
+while [ "$RESULT" -ne 3 ]; do
     echo "Service starting..."
     sleep 5s
+    echo $RESULT
 done
+
 
 
 echo -e "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo -e "Deploying PHP service to EKS done!\nCheck domain: $DOMAIN"
 echo -e "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-
