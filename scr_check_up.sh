74e65b98a175d3e86eaf1e8614ff6036bce83fa1
Andrey Reshetnikov <a.s.reshetnikov@s7.ru>
2020-05-27 12:03:28 +0300

    change scr_check_up.sh

--- scr_check_up.sh
+++ scr_check_up.sh
@@ -11,7 +11,7 @@ do
     echo "Domain creating..."
     sleep 5s
     DOMAIN="$(kubectl get svc |grep LoadBalancer |awk '{print $4}')"
-#    echo $DOMAIN > /dev/null
+    echo $DOMAIN > /dev/null
 done
 
 POD_NAME="$(kubectl get pod |grep 'php-' |awk '{print $1}')"
@@ -22,10 +22,10 @@ do
     sleep 5s
     curl -s $DOMAIN |grep HOSTNAME | grep -c $POD_NAME > /dev/null
     if [ $? -eq 0 ]; then break; fi
- #   echo $RESULT > /dev/null
- #   echo $DOMAIN > /dev/null
- #   echo $POD_NAME > /dev/null
- #   DOMAIN="$(kubectl get svc |grep LoadBalancer |awk '{print $4}')"
+    echo $RESULT > /dev/null
+    echo $DOMAIN > /dev/null
+    echo $POD_NAME > /dev/null
+    DOMAIN="$(kubectl get svc |grep LoadBalancer |awk '{print $4}')"
 done
 
 
